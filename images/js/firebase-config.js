6fd017848f19ee8c22ae7d2b035c7da94a26c9de
Nasser <nasser.m.95@gmail.com>
2025-10-02 11:11:12 +0400

    installing posthog

---
+++ images/js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBg0eb04wDfvoPgQMMv4-5ULWBMP82egxw",
+    authDomain: "talk-to-me-data.firebaseapp.com",
+    projectId: "talk-to-me-data",
+    storageBucket: "talk-to-me-data.firebasestorage.app",
+    messagingSenderId: "143950257839",
+    appId: "1:143950257839:web:56d56865ac3f168ada3e5a"
+};
+
+export { firebaseConfig };
